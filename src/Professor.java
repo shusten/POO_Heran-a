159bede015cea72bad36164cac94297f9ab76d30
Luis Villas <shusten@hotmail.com>
2019-12-10 15:33:42 -0300

    Atualizado método receberAum

--- src/Professor.java
+++ src/Professor.java
@@ -2,8 +2,8 @@ public class Professor extends Pessoa {
     private String especialidade;
     private float salario;
 
-    public void receberAum() {
-
+    public void receberAum(float aum) {
+        this.salario += aum;
     }
 
     public String getEspecialidade() {
