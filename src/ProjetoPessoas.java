f5058f647e827de8d64144580d95d4ffe4d0296c
Luis Villas <shusten@hotmail.com>
2019-12-10 15:58:54 -0300

    Finalizado aula PT1

--- src/ProjetoPessoas.java
+++ src/ProjetoPessoas.java
@@ -13,9 +13,15 @@ public class ProjetoPessoas {
         p1.setSexo("Masculino");
         p4.setSexo("Feminino");
 
+        p2.setIdade(18);
+
         p2.setCurso("Informática");
         p3.setSalario(2500.75f);
         p4.setSetor("Estoque");
+
+        //p1.receberAum(550.20f);
+        //p2.mudarTrabalho();
+        //p4.cancelarMatr();
 
         System.out.println(p1.toString());
         System.out.println(p2.toString());
